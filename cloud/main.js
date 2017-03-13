66faee295e46bc406bb4f185d4898c5de3c89efa
João Pedro Segurado <jpmsegurado@gmail.com>
2017-03-13 09:07:37 -0300

    Cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -39,7 +39,7 @@ var sendNotification = function(message, data){
 };
 
 
-Parse.Cloud.define('LostPet', function(request, response) {
+Parse.Cloud.afterSave('LostPet', function(request, response) {
 
   sendNotification('Mensagem arrobado', request.object);
 
