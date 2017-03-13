ecdee18c8c1fbc8c03880ce5ad8f5fc3fdf873e6
João Pedro Segurado <jpmsegurado@gmail.com>
2017-03-13 10:24:41 -0300

    Cloud Code bla

--- cloud/main.js
+++ cloud/main.js
@@ -63,13 +63,61 @@ Parse.Cloud.afterSave('FoundPet', function(request) {
 
 
 Parse.Cloud.define('bla', function(request, response) {
-  var query = new Parse.Query('LostPet');
-  return query.find({
-    success: function(res){
-      response.success(res);
-    },
-    error: function(err) {
-      response.error(err.message);
-    }
-  });
+  
+  try {
+    var sendNotification = function(message, data, ids){
+      var tags = [];
+      var headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Basic Nzg4NzgxNDYtZTYxOS00MmM2LTkwNDQtNTkxZDRkMTk0Y2U3"
+      };
+
+
+      var data = {
+        // included_player_ids: ids,
+        included_segments:["All"],
+        app_id: "11ccaccc-f923-4474-b1e8-c4b3b6dfa1da",
+        contents: {"en": message},
+        data: data
+      };
+
+      var options = {
+        host: "onesignal.com",
+        port: 443,
+        path: "/api/v1/notifications",
+        method: "POST",
+        headers: headers
+      };
+
+      var https = require('https');
+      var req = https.request(options, function(res) {
+        res.on('data', function(d) {
+          console.log(JSON.parse(d))
+        });
+      });
+
+      req.on('error', function(e) {
+        console.log(JSON.parse(e))
+      });
+
+      req.write(JSON.stringify(data));
+      req.end();
+    };
+    
+    var lat = request.object.location.latitude;
+    var long = request.object.location.longitude;
+    var point = new Parse.GeoPoint({ latitude: lat, longitude: long }); 
+    var query = new Parse.Query('LostPet');
+    query.withinKilometers('location', point, 25);
+    return query.find({
+      success: function(res){
+        sendNotification('Mensagem arrobado', res);
+      },
+      error: function(err) {
+        console.log(err.message);
+      }
+    });
+  } catch(e) {
+    console.log(e.message); 
+  }
 })
