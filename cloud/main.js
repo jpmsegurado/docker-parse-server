1f2b219956b8a42e8e761280057ec769fb83f2d1
João Pedro Segurado <jpmsegurado@gmail.com>
2017-03-13 09:18:21 -0300

    Cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,45 +1,44 @@
 var _ = require('lodash');
 
-var sendNotification = function(message, data){
-  var tags = [];
-  var headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Basic Nzg4NzgxNDYtZTYxOS00MmM2LTkwNDQtNTkxZDRkMTk0Y2U3"
-  };
-
-
-  var data = {
-    included_segments:["All"],
-    app_id: "11ccaccc-f923-4474-b1e8-c4b3b6dfa1da",
-    contents: {"en": message},
-    data: data
-  };
-
-  var options = {
-    host: "onesignal.com",
-    port: 443,
-    path: "/api/v1/notifications",
-    method: "POST",
-    headers: headers
-  };
-
-  var https = require('https');
-  var req = https.request(options, function(res) {
-    res.on('data', function(data) {
-      console.log(JSON.parse(data))
-    });
-  });
-
-  req.on('error', function(e) {
-    console.log(JSON.parse(e))
-  });
-
-  req.write(JSON.stringify(data));
-  req.end();
-};
-
-
 Parse.Cloud.afterSave('LostPet', function(request) {
+
+  var sendNotification = function(message, data){
+    var tags = [];
+    var headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Basic Nzg4NzgxNDYtZTYxOS00MmM2LTkwNDQtNTkxZDRkMTk0Y2U3"
+    };
+
+
+    var data = {
+      included_segments:["All"],
+      app_id: "11ccaccc-f923-4474-b1e8-c4b3b6dfa1da",
+      contents: {"en": message},
+      data: data
+    };
+
+    var options = {
+      host: "onesignal.com",
+      port: 443,
+      path: "/api/v1/notifications",
+      method: "POST",
+      headers: headers
+    };
+
+    var https = require('https');
+    var req = https.request(options, function(res) {
+      res.on('data', function(data) {
+        console.log(JSON.parse(data))
+      });
+    });
+
+    req.on('error', function(e) {
+      console.log(JSON.parse(e))
+    });
+
+    req.write(JSON.stringify(data));
+    req.end();
+  };
 
   sendNotification('Mensagem arrobado', request.object);
 
