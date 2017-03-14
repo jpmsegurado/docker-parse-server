dadb76ff9e3bb92588d7e8442b8b51b2d6c487e3
João Pedro Segurado <jpmsegurado@gmail.com>
2017-03-14 08:49:39 -0300

    Cloud Code bla

--- cloud/main.js
+++ cloud/main.js
@@ -58,10 +58,18 @@ Parse.Cloud.afterSave('FoundPet', function(request, response) {
           }
         });
 
-        sendNotification('Há pets encontrados próximo ao local onde você perdeu seu pet', {
-          test: request.object.get('user').toJSON().objectId,
-          ids: players
-        }, players);
+        let usersQuery = new Parse.Query(Parse.User);
+        usersQuery.withinKilometers('address_point', point, 25);
+
+        usersQuery.find().then(function(users) {
+          const parsed = _.map(users, function(u){ return u.toJSON() });
+          _.forEach(parsed, function(usr) { !!usr.player_id && players.push(usr.player_id) });
+          sendNotification('Há pets encontrados próximo ao local onde você perdeu seu pet', {
+            test: request.object.get('user').toJSON().objectId,
+            ids: players
+          }, players);
+        });
+
       },
       error: function(err) {
         console.log('error', err.message);
@@ -76,69 +84,3 @@ Parse.Cloud.afterSave('FoundPet', function(request, response) {
 });
 
 
-Parse.Cloud.define('bla', function(request, response) {
-  
-  try {
-    var sendNotification = function(message, data, ids){
-      var tags = [];
-      var headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Basic Nzg4NzgxNDYtZTYxOS00MmM2LTkwNDQtNTkxZDRkMTk0Y2U3"
-      };
-
-
-      var data = {
-        // included_player_ids: ids,
-        included_segments:["All"],
-        app_id: "11ccaccc-f923-4474-b1e8-c4b3b6dfa1da",
-        contents: {"en": message},
-        data: data
-      };
-
-      var options = {
-        host: "onesignal.com",
-        port: 443,
-        path: "/api/v1/notifications",
-        method: "POST",
-        headers: headers
-      };
-
-      var https = require('https');
-      var req = https.request(options, function(res) {
-        res.on('data', function(d) {
-          console.log(JSON.parse(d))
-        });
-      });
-
-      req.on('error', function(e) {
-        console.log(JSON.parse(e))
-      });
-
-      req.write(JSON.stringify(data));
-      req.end();
-    };
-
-    
-    var lat = request.params.location.latitude;
-    var long = request.params.location.longitude;
-    var point = new Parse.GeoPoint(lat, long); 
-    var query = new Parse.Query('LostPet');
-    query.withinKilometers('location', point, 25);
-    query.find({
-      success: function(res){
-        sendNotification('Mensagem arrobado', res);
-        response.success(res);
-      },
-      error: function(err) {
-        response.success({
-          error: err,
-          point: point,
-          url: Parse.serverURL,
-          app_id: Parse.app_id
-        });
-      }
-    });
-  } catch(e) {
-    response.success(e.message); 
-  }
-})
